b80ce20460e1786cf80cd00ab1ff095f03ca057b
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2016-11-07 18:28:33 +0100

    Add knexfile with db connection details

---
+++ knexfile.js
@@ -0,0 +1,41 @@
+module.exports = {
+
+  development: {
+    client:     'postgresql',
+    connection: {
+      database: 'jtl-jobs',
+      user:     'alexander',
+    },
+  },
+
+  staging: {
+    client:     'postgresql',
+    connection: {
+      database: 'jtl-jobs',
+      user:     'jtl',
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+    },
+  },
+
+  production: {
+    client:     'postgresql',
+    connection: {
+      database: 'jtl-jobs',
+      user:     'jtl',
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+    },
+  },
+
+};
