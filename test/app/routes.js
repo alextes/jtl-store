54b5749c0c1c18798a398c28e5917807d34a6c67
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2016-11-20 02:00:46 +0100

    Clarify health route test

--- test/app/routes.js
+++ test/app/routes.js
@@ -2,8 +2,9 @@
 import test from 'ava';
 import { checkHealth } from '../../src/app/routes';
 
-test('should respond 200 - OK to GET /', async (t) => {
-  const ctx = { response: { status: 500 } };
+test('should respond \'200 - OK\' to GET /', async (t) => {
+  // normally checkHealth is passed a koa context
+  const ctx = { response: { status: null } };
   checkHealth(ctx);
 
   t.is(ctx.response.status, 200);
