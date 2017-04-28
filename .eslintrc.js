1ab468da542c1646b2805d95fa9455b502ba9850
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2017-04-28 21:14:21 +0200

    Improve scripts and eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,26 @@
 module.exports = {
-  "parser": "babel-eslint",
-  "extends": "airbnb-base",
-  "plugins": [
-    "jest"
-    "flowtype",
-    "promise"
+  env: {
+    jest: true,
+  },
+  parser: 'babel-eslint',
+  extends: 'airbnb-base',
+  plugins: [
+    'jest'
+    'flowtype',
+    'promise'
   ],
-  "rules": {
-    "import/no-extraneous-dependencies": "off",
-    "key-spacing": [
-      "error",
+  rules: {
+    'import/no-extraneous-dependencies': 'off',
+    'key-spacing': [
+      'error',
       {
-        "align": "value"
+        align: 'value'
       }
     ],
-    "promise/avoid-new": "warn",
-    "promise/catch-or-return": "error",
-    "promise/param-names": "error",
-    "promise/no-promise-in-callback": "warn",
-    "promise/no-callback-in-promise": "warn"
+    'promise/avoid-new': 'warn',
+    'promise/catch-or-return': 'error',
+    'promise/param-names': 'error',
+    'promise/no-promise-in-callback': 'warn',
+    'promise/no-callback-in-promise': 'warn'
   }
 }
